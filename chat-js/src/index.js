8ad9d336d2b4e93bb49559f0307898782be8d233
c1b3r-p3d1a <hugo_co@icloud.com>
2023-01-14 19:48:04 +0100

    v 0.1

--- chat-js/src/index.js
+++ chat-js/src/index.js
@@ -3,8 +3,13 @@ const path = require('path');
 
 const app = express();
 
+const server = require('http').Server(app);
+const socketio = require('socket.io')(server);
 
-app.listen(3000, () => {
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(3000, () => {
     console.log('Server listening on port 3000');
 })
 
